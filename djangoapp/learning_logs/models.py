f3f1c2e4ff6401a2e9a69d30a0fe39e151b0f860
guilherme.felipe <guilherme.felipe@retta.com.br>
2024-02-26 11:05:25 -0300

    primeira model e sua migration

--- djangoapp/learning_logs/models.py
+++ djangoapp/learning_logs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Topic(models.Model):
+    """ Um Assunto Sobre o qual o usuario esta aprendendo """
+
+    text = models.CharField(max_length=200)
+
+    date_added = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        """ Devolve uma representação em string do modelo. """
+        return self.text
